da194b3ef2aac3d9ac31e6363e8788859cc0b488
odile-k <159757436+odile-k@users.noreply.github.com>
2024-02-12 09:24:32 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,17 +6,12 @@ from urllib.request import urlopen
 import sqlite3
                                                                                                                                        
 app = Flask(__name__) 
-
-  @app.route("/contact/")
-def MaPremiereAPI():
-    return "<h2>Ma page de contact</h2>"
-
                                                                                                                                        
 @app.route('/')
 def hello_world():
     return render_template('hello.html') #Comm
-  
-  @app.route("/contact/")
+
+@app.route("/contact/")
 def MaPremiereAPI():
     return "<h2>Ma page de contact</h2>"
 
