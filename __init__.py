a81ff89fd11a65f0b7a89abee1b05d67cedb85c6
odile-k <159757436+odile-k@users.noreply.github.com>
2024-02-12 09:18:08 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,18 +5,19 @@ from datetime import datetime
 from urllib.request import urlopen
 import sqlite3
                                                                                                                                        
-app = Flask(__name__)
+app = Flask(__name__) 
 @app.route("/contact/")
 def MaPremiereAPI():
     return "<h2>Ma page de contact</h2>"
+
                                                                                                                                        
 @app.route('/')
 def hello_world():
     return render_template('hello.html') #Comm
-  
   @app.route("/contact/")
 def MaPremiereAPI():
     return "<h2>Ma page de contact</h2>"
+
   
 if __name__ == "__main__":
   app.run(debug=True)
