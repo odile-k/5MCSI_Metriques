91ff17e63e1ae038f205f6777a11face277ebb9d
odile-k <159757436+odile-k@users.noreply.github.com>
2024-02-12 09:57:55 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -30,6 +30,34 @@ def meteo():
 def mongraphique():
     return render_template("graphique.html")
 
+from flask import Flask, render_template
+
+app = Flask(__name__) 
+
+@app.route('/')
+def hello_world():
+    return render_template('hello.html')
+
+@app.route("/histogramme/")
+def histogramme():
+    # Supposons que vous ayez déjà collecté les données de température dans une liste nommée "temperatures"
+    temperatures = [15, 17, 18, 19, 20, 22, 24, 25, 23, 21, 20, 18, 17, 16, 15, 14]
+
+    # Construction du script JavaScript pour le graphique d'histogramme
+    script = """
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Jour', 'Température'],
+          """
+    for i, temp in enumerate(temperatures):
+        script += "['Jour {}', {}],".format(i+1, temp)
+    script += "
+
   
 if __name__ == "__main__":
   app.run(debug=True)
